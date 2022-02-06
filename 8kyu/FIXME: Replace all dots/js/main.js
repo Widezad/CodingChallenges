7bb15e5bddc94898028e61842f60b9fecc9cd774
widezad <Widezad@gmail.com>
2022-02-06 08:48:29 -0500

    Add coding challenge for 6-02-22

---
+++ 8kyu/FIXME: Replace all dots/js/main.js
@@ -0,0 +1,17 @@
+/*
+The code provided is supposed replace all the dots . 
+in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+*/
+
+const replaceDots = (str) => {
+   return str.split('.').join('-')
+}
+console.log(replaceDots("one.two.three")) //"one-two-three"
