b5011473bfaa974aad843930cb01656fa58b5930
widezad <widezad@gmail.com>
2022-04-11 13:17:47 -0400

    update coding challenge for 4-11-2022

--- 8kyu/April-11-2022/js/main.js
+++ 8kyu/April-11-2022/js/main.js
@@ -14,3 +14,19 @@ const items = []
 //put parentheses around curly braces.
 items.push ({ a: 'b', c: 'd'})
 
+/*
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same 
+order.
+
+"h,o,l,a"
+*/
+function printArray(array){
+    //Use join method
+    return array.join(',');
+
+  }
+  console.log(printArray(["h","o","l","a"]))
