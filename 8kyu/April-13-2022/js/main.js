f37b801c5f486ec2c754ae1e03a71a8c553a1f0d
widezad <widezad@gmail.com>
2022-04-13 21:35:18 -0400

    Add To Codding Challegnge for 4-13-2022

---
+++ 8kyu/April-13-2022/js/main.js
@@ -0,0 +1,29 @@
+/*
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the 
+average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare
+ your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating 
+the average point you may add your point to the given array!
+
+*/
+
+//Write out function
+function betterThanAverage(classPoints, yourPoints) {
+
+    //declare variable to calculate and compare score with reduce method
+    let average = classPoints.reduce((acc, curr) => acc + curr) / classPoints.length < yourPoints
+
+    //retrun average
+    return average 
+}
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88]))
+console.log(betterThanAverage([2, 3], 5))
+console.log(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28]))
+
