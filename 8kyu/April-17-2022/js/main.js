6b2d2a3203cda452d85fcc82adb283f7414800a3
widezad <widezad@gmail.com>
2022-04-17 00:11:03 -0400

    Add coding challenge for 4-17-2022

--- 8kyu/April-17-2022/js/main.js
+++ 8kyu/April-17-2022/js/main.js
@@ -26,4 +26,4 @@ function capitalizeWord(word) {
    return word.charAt(0).toUpperCase() + word.slice(1);
 }
 console.log(capitalizeWord('lol')) //Lol
-console.log(capitalizeWord('w')) //W
+console.log(capitalizeWord('w')) //W 
