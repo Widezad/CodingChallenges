b929971392605bca4476020e03233000fdf2eafe
widezad <widezad@gmail.com>
2022-04-21 16:33:18 -0400

    Add Coding Challenge to 4-21-2022

--- 8kyu/April-21-2022/js/main.js
+++ 8kyu/April-21-2022/js/main.js
@@ -10,18 +10,21 @@ rps('scissors','rock') // Player 2 won!
 rps('paper','paper') // Draw!
 */
 const rps = (p1, p2) => {
+    //create condition for when player 1 wins 
     if(p1 === 'scissors' && p2 === 'paper' 
     || p1 === 'paper' && p2 === 'rock' 
     || p1 === 'rock' && p2 === 'scissors') {
-        return `p1 wins`
+        return `Player 1 won!`
 
+    //create condition for when player 2 wins 
     }else if(p1 === 'paper' && p2 === 'scissors' 
     || p1 ==='rock' && p2 === 'paper' 
     || p1 === 'scissors' && p2 === 'rock') {
-        return `p2 wins`
+        return `Player 2 won!`
 
+    //create condition for when there's a tie
     }else if(p1 === p2){
-        return `Draw`
+        return `Draw!`
     }
 };
 console.log(rps('scissors', 'rock'))
