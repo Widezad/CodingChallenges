287b124957eb42998d6b8955c932e0fbe5f99ffa
widezad <widezad@gmail.com>
2022-04-27 19:09:50 -0400

    Add coding challenge to 4-27-2022

--- 8kyu/April-27-2022/js/main.js
+++ 8kyu/April-27-2022/js/main.js
@@ -15,3 +15,19 @@ function getAge(inputString){
     return parseInt(inputString.charAt(0))
 }
 console.log(getAge('1'))
+
+
+/*
+Given a non-negative integer, 3 for example, return a string 
+with a murmur: "1 sheep...2 sheep...3 sheep...". Input will 
+always be valid, i.e. no negative integers.
+*/
+let countSheep = function (num){
+    //your code here
+    let sheep = "";
+    for(let i = 1; i <= num; i++) {
+        sheep +=  `${i} sheep...`;
+    }
+    return sheep
+}
+console.log(countSheep(3))
