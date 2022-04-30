66adde430e738b5f9d2439c0224bc30b2e2d8825
widezad <widezad@gmail.com>
2022-04-30 13:45:24 -0400

    Add coding challenge for 3-30-22.

---
+++ March-30-2022/js/main.js
@@ -0,0 +1,11 @@
+/*
+Implement a function which multiplies two numbers.
+*/
+
+//Write function and add two parameters
+function multiplyTwo(n1, n2) {
+    // return and multiply parameters
+    return n1 * n2
+}
+//call function and print to console
+console.log(multiplyTwo(20, 2)) //40
