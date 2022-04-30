647f329c3609bffb6ddf5e8172efd8a87f673978
widezad <widezad@gmail.com>
2022-04-29 21:12:45 -0400

    Add Coding Challlenge for 4-29-22

---
+++ 8kyu/April-29-2022/js/main.js
@@ -0,0 +1,37 @@
+/*
+Our football team finished the championship. The result of 
+each match look like "x:y". Results of all matches are recorded
+ in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the 
+points of our team in the championship. Rules for counting 
+points for each match:
+
+if x > y: 3 points
+if x < y: 0 point
+if x = y: 1 point
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+function points(games) {
+    // create variable to add to
+    let totalPoints = 0;
+    
+    for(let i = 0; i < games.length; i++) {
+
+        let xScore = Number(games[i].charAt(0));
+        let yScore = Number(games[i].charAt(2));
+
+        if(xScore > yScore) totalPoints+=3
+        if(xScore < yScore) totalPoints +=0
+        if(xScore  === yScore) totalPoints+=1
+    }
+    return totalPoints
+}
+console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]))
