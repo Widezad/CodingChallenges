72a2874ab7a81c6e9e3b7cbca822f5a543759616
widezad <widezad@gmail.com>
2022-05-12 17:40:35 -0400

    add new coding challenge for 05-12-2022

---
+++ 8kyu/Convert a Boolean to a String/js/main.js
@@ -0,0 +1,15 @@
+/*
+Implement a function which convert the given boolean value into 
+its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+/*(booleanToString(true), "true", 'When we pass in true, we 
+want the string "true" as output'); */
+
+function booleanToString(b){
+    //Use "String" method to convert ot string.
+    return String(b)
+}
+console.log(booleanToString(true))
