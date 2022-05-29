2001fb4e738a5d5098291e5d15529ebab5d6c7b9
widezad <Widezad@gmail.com>
2022-05-28 23:54:07 -0400

    Add coding challenge for 05-28-22

---
+++ 8kyu/Reversed Words/js/main.js
@@ -0,0 +1,16 @@
+/*
+Complete the solution so that it reverses all of the words within the string 
+passed in.
+
+Example:
+
+"The greatest victory is that which requires no battle" --> 
+"battle no requires which that is victory greatest The"
+*/
+
+function reverseWords(str){
+    return str.split(' ').reverse().joint(' ')
+
+}
+console.log(reverseWords("The greatest victory is that which requires no battle"))
+//"battle no requires which that is victory greatest The"
