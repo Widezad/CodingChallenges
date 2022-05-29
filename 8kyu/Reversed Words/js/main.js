1236435e8655eb2786c4090cd798795a5c8e64d7
widezad <Widezad@gmail.com>
2022-05-28 23:55:08 -0400

    Update challenge for 05-28-22

--- 8kyu/Reversed Words/js/main.js
+++ 8kyu/Reversed Words/js/main.js
@@ -9,7 +9,7 @@ Example:
 */
 
 function reverseWords(str){
-    return str.split(' ').reverse().joint(' ')
+    return str.split(' ').reverse().join(' ')
 
 }
 console.log(reverseWords("The greatest victory is that which requires no battle"))
