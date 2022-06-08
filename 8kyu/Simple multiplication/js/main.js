a25fb3304e4c0b4ec15e5c48200bd3d8b5ff3e97
widezad <Widezad@gmail.com>
2022-06-07 22:02:13 -0400

    Update coding challenge

--- 8kyu/Simple multiplication/js/main.js
+++ 8kyu/Simple multiplication/js/main.js
@@ -38,3 +38,7 @@ console.log(simpleMultiplication(1)) //9
 console.log(simpleMultiplication(8)) //64
 console.log(simpleMultiplication(4)) //32
 console.log(simpleMultiplication(5)) //45
+
+//Refactored 
+
+const simpleMultiplication = (number) => number % 2 === 0 ? number * 8 : number * 9
