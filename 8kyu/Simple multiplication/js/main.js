ab69879796600326f4f2f7e380e81f83728a288e
widezad <Widezad@gmail.com>
2022-06-07 21:49:54 -0400

    Update coding challenge

--- 8kyu/Simple multiplication/js/main.js
+++ 8kyu/Simple multiplication/js/main.js
@@ -3,3 +3,10 @@ Simple Multiplication
 This kata is about multiplying a given number
 by eight if it is an even number and by nine otherwise.
 */
+
+
+//To do 
+/*
+Create a coditional that returns the given number by eight and 
+multiplies by nine if not even.
+*/
