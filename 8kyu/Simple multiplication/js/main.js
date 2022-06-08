9610c66bc6371aaa1bfa6b9382434f7838f7afd0
widezad <Widezad@gmail.com>
2022-06-07 21:51:35 -0400

    update coding challenge

--- 8kyu/Simple multiplication/js/main.js
+++ 8kyu/Simple multiplication/js/main.js
@@ -10,3 +10,11 @@ by eight if it is an even number and by nine otherwise.
 Create a coditional that returns the given number by eight and 
 multiplies by nine if not even.
 */
+
+//Notes
+/*
+Always a number
+Always positive 
+if enter 2 should return 16
+if enter 1 should return 9
+*/
