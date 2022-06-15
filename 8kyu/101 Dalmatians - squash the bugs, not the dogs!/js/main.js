9ffebee65ee3dad269eafb4c580151ba56f70dd2
widezad <Widezad@gmail.com>
2022-06-14 21:24:25 -0400

    Update challenge

--- 8kyu/101 Dalmatians - squash the bugs, not the dogs!/js/main.js
+++ 8kyu/101 Dalmatians - squash the bugs, not the dogs!/js/main.js
@@ -15,10 +15,8 @@ function howManyDalmations(number) {
   
     let dogs = ['Hardly any', "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
     
-    let respond = number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number === 101 ? dogs[3] : dogs[2] ))
+    return number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number === 101 ? dogs[3] : dogs[2] ))
     
-  return respond
-
 }
 //test cases
 console.log(howManyDalmations(26)) //More than a handful!
