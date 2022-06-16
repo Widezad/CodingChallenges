8e7df0bc369998a7b152a33ea1ce933b23ae3e7d
widezad <Widezad@gmail.com>
2022-06-15 22:27:15 -0400

    Update challenge

--- 8kyu/Squash the bugs/js/main.js
+++ 8kyu/Squash the bugs/js/main.js
@@ -37,3 +37,8 @@ function findLongest(str) {
     return longest
 }
 //test cases
+console.log(findLongest("The quick white fox jumped around the massive dog")) //7
+console.log(findLongest("Take me to tinseltown with you")) //10
+console.log(findLongest("Sausage chops")) //7
+console.log(findLongest("Wind your body and wiggle your belly")) //6
+console.log(findLongest("Lets all go on holiday")) //7
