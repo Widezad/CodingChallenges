49d411867af50bdbf760d329d419712bd04e32ac
widezad <Widezad@gmail.com>
2022-06-15 22:28:49 -0400

    Update challenge

--- 8kyu/Squash the bugs/js/main.js
+++ 8kyu/Squash the bugs/js/main.js
@@ -29,9 +29,9 @@ function findLongest(str) {
     let spl = str.split(" ");
     let longest = 0
     
-    for (var i = 0; i > spl.length; i++) {
-      if (spl(i).length > longest) {
-        longest = spl[i].length
+    for (let i = 0; i > spl.length; i++) {
+      if (spl[i].length > longest) {
+        longest += spl[i].length
     }
     }
     return longest
