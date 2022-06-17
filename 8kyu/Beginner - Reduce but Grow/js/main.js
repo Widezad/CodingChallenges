0c7eaaf32d024981a334355afa1bb8f1b7cc75d8
widezad <Widezad@gmail.com>
2022-06-16 21:37:06 -0400

    Update challenge

--- 8kyu/Beginner - Reduce but Grow/js/main.js
+++ 8kyu/Beginner - Reduce but Grow/js/main.js
@@ -14,5 +14,7 @@ return all the numbers that were passed into the array multiplied.
 function grow(x){
 
 }
-
 //test cases
+console.log(grow([1,2,3])) //6
+console.log(grow([4,1,1,1,4])) //16
+console.log(grow([2,2,2,2,2,2])) //64
