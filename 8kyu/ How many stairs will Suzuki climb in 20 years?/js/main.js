ad585e0acfb26dfdec08334d9f98704fe1781d5b
widezad <Widezad@gmail.com>
2022-06-18 10:09:10 -0400

    Update coding challenge

--- 8kyu/ How many stairs will Suzuki climb in 20 years?/js/main.js
+++ 8kyu/ How many stairs will Suzuki climb in 20 years?/js/main.js
@@ -29,3 +29,5 @@ sunday = [6737, 7244, 5776, 9826, 7057, 9247, 5842, 5484, 6543,
 Your function should return the 20 year estimate of the stairs 
 climbed using the formula above.
 */
+
+//write out function
