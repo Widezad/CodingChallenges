68f140ae9c65a0d45e1b91e539931c3b7f035c3d
widezad <Widezad@gmail.com>
2022-06-18 10:06:43 -0400

    Update coding challenge

--- 8kyu/ How many stairs will Suzuki climb in 20 years?/js/main.js
+++ 8kyu/ How many stairs will Suzuki climb in 20 years?/js/main.js
@@ -10,10 +10,14 @@ estimating the number he might climb in 20.
 
 20_year_estimate = one_year_total * 20
 
-You will receive the following data structure representing the stairs Suzuki logged in a year. You will have all data for the entire year so regardless of how it is logged the problem should be simple to solve.
+You will receive the following data structure representing the 
+stairs Suzuki logged in a year. You will have all data for the 
+entire year so regardless of how it is logged the problem should 
+be simple to solve.
 
 stairs = [sunday,monday,tuesday,wednesday,thursday,friday,saturday]
-Make sure your solution takes into account all of the nesting within the stairs array.
+Make sure your solution takes into account all of the nesting 
+within the stairs array.
 
 Each weekday in the stairs array is an array.
 */
