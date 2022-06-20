a9715be27806201581a8b4373b1aa920e6eddd3e
widezad <Widezad@gmail.com>
2022-06-19 22:13:52 -0400

    Update coding challenge

--- 8kyu/Expressions Matter/js/main.js
+++ 8kyu/Expressions Matter/js/main.js
@@ -1,2 +1,7 @@
 /*
+Task
+Given three integers a ,b ,c, return the largest number obtained after inserting the 
+following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum 
+Obtained
 */
