b93f52fd1da2f09a0cec0a231c0744950cc68b05
widezad <Widezad@gmail.com>
2022-06-19 22:17:39 -0400

    Update coding challenge

--- 8kyu/Expressions Matter/js/main.js
+++ 8kyu/Expressions Matter/js/main.js
@@ -37,4 +37,16 @@ expressionsMatter(9,1,1)  ==>  return 18
 Explanation:
 After placing signs and brackets, the Maximum value obtained from the expression is 
 9 * (1+1) = 18. ___# Task
+
+Given three integers a ,b ,c, return the largest number obtained after inserting the
+following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum
+Obtained
+Example
+With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+
+1 * (2 + 3) = 5
+1 * 2 * 3 = 6
+1 + 2 * 3 = 7
+(1 + 2) * 3 = 9
 */
