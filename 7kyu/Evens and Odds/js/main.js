11b3fcaef8f47deda32e69ad8020afaada325a1b
widezad <Widezad@gmail.com>
2022-06-20 12:17:16 -0400

    Update coding challege

--- 7kyu/Evens and Odds/js/main.js
+++ 7kyu/Evens and Odds/js/main.js
@@ -18,5 +18,14 @@ always a number
 //write out function
 function evensAndOdds(num){
 	//your code here
+    if(num % 2 === 0) {
+        return num.toString(2)
+    }
 }
 //test cases
+console.log(evensAndOdds(2)) //10
+console.log(evensAndOdds(13)) //d
+console.log(evensAndOdds(47)) //2f
+console.log(evensAndOdds(0)) //0
+console.log(evensAndOdds(12800)) //11001000000000
+console.log(evensAndOdds(8172381723)) //1e71ca61b
