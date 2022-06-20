ba63de9f942d7d34fa9bb22c438c0a176f8dc747
widezad <Widezad@gmail.com>
2022-06-19 22:15:16 -0400

    Update coding challenge

--- 8kyu/Expressions Matter/js/main.js
+++ 8kyu/Expressions Matter/js/main.js
@@ -12,4 +12,13 @@ With the numbers are 1, 2 and 3 , here are some ways of placing signs and bracke
 1 + 2 * 3 = 7
 (1 + 2) * 3 = 9
 So the maximum value that you can obtain is 9.
+
+Notes
+The numbers are always positive.
+The numbers are in the range (1  ≤  a, b, c  ≤  10).
+You can use the same operation more than once.
+It's not necessary to place all the signs and brackets.
+Repetition in numbers may occur .
+You cannot swap the operands. For instance, in the given example you cannot get 
+expression (1 + 3) * 2 = 8.
 */
