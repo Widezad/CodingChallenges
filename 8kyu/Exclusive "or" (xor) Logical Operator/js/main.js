970d7c4aa67714b1d378e25a52fa077cda7e9ee1
widezad <Widezad@gmail.com>
2022-06-22 22:20:01 -0400

    Update challenge

--- 8kyu/Exclusive "or" (xor) Logical Operator/js/main.js
+++ 8kyu/Exclusive "or" (xor) Logical Operator/js/main.js
@@ -6,4 +6,10 @@ operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or"
 (hence the name of this Kata). The exclusive or evaluates two 
 booleans. It then returns true if exactly one of the two 
 expressions are true, false otherwise. For example:
+
+false xor false == false // since both are false
+true xor false == true // exactly one of the two expressions are true
+false xor true == true // exactly one of the two expressions are true
+true xor true == false // Both are true.  "xor" only returns
+ true if EXACTLY one of the two expressions evaluate to true.
 */
