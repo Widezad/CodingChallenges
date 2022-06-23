f4752805cbd88c5a0b9492ed684b84786967b4f3
widezad <Widezad@gmail.com>
2022-06-22 22:22:35 -0400

    Update challenge

--- 8kyu/Exclusive "or" (xor) Logical Operator/js/main.js
+++ 8kyu/Exclusive "or" (xor) Logical Operator/js/main.js
@@ -15,5 +15,6 @@ true if EXACTLY one of the two expressions evaluate to true.
 
 Task
 Since we cannot define keywords in Javascript (well, at least 
-I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+I don't know how to do it), your task is to define a function 
+xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 */
