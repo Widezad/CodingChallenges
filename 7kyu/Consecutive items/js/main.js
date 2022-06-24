74cd90acc392ab604f2d59ee5debeb02730ef066
widezad <Widezad@gmail.com>
2022-06-24 16:32:31 -0400

    Update coding challenge

--- 7kyu/Consecutive items/js/main.js
+++ 7kyu/Consecutive items/js/main.js
@@ -1,5 +1,6 @@
 /*
 You are given a list of unique integers arr, and two integers a 
 and b. Your task is to find out whether or not a and b appear 
-consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+consecutively in arr, and return a boolean value (True if a and 
+b are consecutive, False otherwise).
 */
