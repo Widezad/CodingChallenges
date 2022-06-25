96ad94b1caf2700a7622ec875befd20a7a763354
widezad <Widezad@gmail.com>
2022-06-25 19:48:44 -0400

    Update CHallenge

--- 8kyu/ Square(n) Sum/js/main.js
+++ 8kyu/ Square(n) Sum/js/main.js
@@ -1,3 +1,4 @@
 /*
-Complete the square sum function so that it squares each number passed into it and then sums the results together.
+Complete the square sum function so that it squares each number passed 
+into it and then sums the results together.
 */
