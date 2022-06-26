460ef0bb6b6f99b3fb3df7b6e4cf7727a436f598
widezad <Widezad@gmail.com>
2022-06-26 16:58:46 -0400

    Update Coding Challenge

--- 7kyu/Truthy and Falsy/js/main.js
+++ 7kyu/Truthy and Falsy/js/main.js
@@ -16,3 +16,5 @@ You are given two empty arrays (truthy and falsy) and you
 have to fill this array with at least 5 elements in each 
 which will evaluate to true or false accordingly.
 */
+
+const truthy = [];
