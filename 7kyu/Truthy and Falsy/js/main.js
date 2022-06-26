ff3ece3e5891451147445955563d18ed97449633
widezad <Widezad@gmail.com>
2022-06-26 16:55:22 -0400

    Update Coding Challenge

--- 7kyu/Truthy and Falsy/js/main.js
+++ 7kyu/Truthy and Falsy/js/main.js
@@ -5,4 +5,9 @@ truthy: a value that when evaluated in a boolean context
 returns true
 falsy: a value that when evaluated in a boolean context 
 returns false
+
+MDN links:
+
+https://developer.mozilla.org/en/docs/Glossary/Truthy
+https://developer.mozilla.org/en-US/docs/Glossary/Falsy
 */
