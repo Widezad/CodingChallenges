74a78dce0c13204e82b0028515f05075a9f50a2c
widezad <Widezad@gmail.com>
2022-06-26 16:55:49 -0400

    Update Coding Challenge

--- 7kyu/Truthy and Falsy/js/main.js
+++ 7kyu/Truthy and Falsy/js/main.js
@@ -10,4 +10,7 @@ MDN links:
 
 https://developer.mozilla.org/en/docs/Glossary/Truthy
 https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+
+Your task
+You are given two empty arrays (truthy and falsy) and you have to fill this array with at least 5 elements in each which will evaluate to true or false accordingly.
 */
