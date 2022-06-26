4dd4a75b1d4ce07ef1367d5756f70fdc925ead97
widezad <Widezad@gmail.com>
2022-06-26 16:54:51 -0400

    Update Coding Challenge

--- 7kyu/Truthy and Falsy/js/main.js
+++ 7kyu/Truthy and Falsy/js/main.js
@@ -1,6 +1,8 @@
 /*
 In JavaScript there are truthy and falsy values:
 
-truthy: a value that when evaluated in a boolean context returns true
-falsy: a value that when evaluated in a boolean context returns false
+truthy: a value that when evaluated in a boolean context 
+returns true
+falsy: a value that when evaluated in a boolean context 
+returns false
 */
