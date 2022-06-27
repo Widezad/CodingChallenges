9f1e32baf0c9eb241a979228808424305bc9999f
widezad <Widezad@gmail.com>
2022-06-27 13:08:12 -0400

    test cases

--- 8kyu/Vowel remover/js/main.js
+++ 8kyu/Vowel remover/js/main.js
@@ -21,3 +21,4 @@ function shortcut (string) {
 console.log(shortcut('hello')) //'hll'
 console.log(shortcut('how are you today?')) //'hw r y tdy?'
 console.log(shortcut('complain'))// 'cmpln'
+console.log(shortcut('never'))//'nvr'
