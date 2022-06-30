bfc702e5afe6197d4cdd09aae61f13e336b6eed2
widezad <Widezad@gmail.com>
2022-06-30 17:49:55 -0400

    New Coding Challenge

--- 8kyu/A wolf in sheep's clothing/js/main.js
+++ 8kyu/A wolf in sheep's clothing/js/main.js
@@ -4,7 +4,8 @@ farmer, and are now plagued by wolves which pretend to be sheep.
 Fortunately, you are good at spotting them.
 
 Warn the sheep in front of the wolf that it is about to be eaten. 
-Remember that you are standing at the front of the queue which is at the end of the array:
+Remember that you are standing at the front of the queue which is at 
+the end of the array:
 
 
 */
