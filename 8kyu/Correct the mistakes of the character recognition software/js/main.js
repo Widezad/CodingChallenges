512e1131db2dd4eacab17c0c488cbc89067c8349
widezad <Widezad@gmail.com>
2022-06-29 22:33:25 -0400

    new challenge

--- 8kyu/Correct the mistakes of the character recognition software/js/main.js
+++ 8kyu/Correct the mistakes of the character recognition software/js/main.js
@@ -7,5 +7,6 @@ When documents (especially pretty old ones written with a
 typewriter), are digitised character recognition softwares 
 often make mistakes.
 
-Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+Your task is correct the errors in the digitised text. You only
+have to handle the following mistakes:
 */
