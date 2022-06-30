7bdbb945b0edcb0648a7d9ae17e089b827da5525
widezad <Widezad@gmail.com>
2022-06-30 17:50:30 -0400

    New Coding Challenge

--- 8kyu/A wolf in sheep's clothing/js/main.js
+++ 8kyu/A wolf in sheep's clothing/js/main.js
@@ -7,5 +7,7 @@ Warn the sheep in front of the wolf that it is about to be eaten.
 Remember that you are standing at the front of the queue which is at 
 the end of the array:
 
+[sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+   7      6      5      4      3            2      1
 
 */
