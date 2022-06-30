b686b0d18ce8188349bab67c90cb68a1e8241a44
widezad <Widezad@gmail.com>
2022-06-30 17:58:37 -0400

    New Coding Challenge

--- 8kyu/A wolf in sheep's clothing/js/main.js
+++ 8kyu/A wolf in sheep's clothing/js/main.js
@@ -32,3 +32,5 @@ function warnTheSheep(queue) {
 //test cases
 console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]))
 //"Oi! Sheep number 2! You are about to be eaten by a wolf!"
+console.log(warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"]))
+//"Oi! Sheep number 5! You are about to be eaten by a wolf!"
