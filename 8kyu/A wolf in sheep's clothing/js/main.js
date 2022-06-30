7c00bf0a25df5b35d91c16fc6fc6d969778603b1
widezad <Widezad@gmail.com>
2022-06-30 17:48:59 -0400

    New Coding Challenge

--- 8kyu/A wolf in sheep's clothing/js/main.js
+++ 8kyu/A wolf in sheep's clothing/js/main.js
@@ -1,4 +1,5 @@
 /*
 Wolves have been reintroduced to Great Britain. You are a sheep 
-farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+farmer, and are now plagued by wolves which pretend to be sheep. 
+Fortunately, you are good at spotting them.
 */
