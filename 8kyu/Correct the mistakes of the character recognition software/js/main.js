9b12f99cbff58a38ab182602346c67f44468a15b
widezad <Widezad@gmail.com>
2022-06-29 22:31:57 -0400

    new challenge

--- 8kyu/Correct the mistakes of the character recognition software/js/main.js
+++ 8kyu/Correct the mistakes of the character recognition software/js/main.js
@@ -3,5 +3,6 @@ Character recognition software is widely used to digitise
 printed texts. Thus the texts can be edited, searched and stored 
 on a computer.
 
-When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+When documents (especially pretty old ones written with a 
+typewriter), are digitised character recognition softwares often make mistakes.
 */
