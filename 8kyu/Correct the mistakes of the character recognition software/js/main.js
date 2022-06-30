fd1995a6f91bca643583dcd038538ecc755687db
widezad <Widezad@gmail.com>
2022-06-29 22:42:39 -0400

    new challenge

--- 8kyu/Correct the mistakes of the character recognition software/js/main.js
+++ 8kyu/Correct the mistakes of the character recognition software/js/main.js
@@ -18,8 +18,14 @@ The test cases contain numbers only by mistake.
 */
 
 //write out functionc
-function correct(string){
-	// your code here
-}
+function correct(s){
+    s = s.split('');
+    for (var i = 0; i < s.length; i++) {
+      if (s[i] === '5') s[i] = 'S';
+      else if (s[i] === '0') s[i] = 'O';
+      else if (s[i] === '1') s[i] = 'I';
+      }
+    return s.join('');
+  }
 
 //test cases
