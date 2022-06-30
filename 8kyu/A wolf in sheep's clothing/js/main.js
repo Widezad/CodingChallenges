e8ef82572c3a3d85a9a7850adffbada170741c1d
widezad <Widezad@gmail.com>
2022-06-30 18:04:49 -0400

    New Coding Challenge

--- 8kyu/A wolf in sheep's clothing/js/main.js
+++ 8kyu/A wolf in sheep's clothing/js/main.js
@@ -26,7 +26,12 @@ Output: "Pls go away and stop eating my sheep"
 
 //Write out function
 function warnTheSheep(queue) {
-
+    if (queue[queue.length -1] === 'wolf') {
+      return 'Pls go away and stop eating my sheep';
+      } else {
+       let index = queue.findIndex( (x) => x == 'wolf' );
+       return `Oi! Sheep number ${queue.length - index - 1}! You are about to be eaten by a wolf!`;
+      }
 }
 
 //test cases
