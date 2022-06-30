46e3ac1e0ae578a44a8388be915f635a3538da6f
widezad <Widezad@gmail.com>
2022-06-29 22:32:12 -0400

    new challenge

--- 8kyu/Correct the mistakes of the character recognition software/js/main.js
+++ 8kyu/Correct the mistakes of the character recognition software/js/main.js
@@ -4,5 +4,6 @@ printed texts. Thus the texts can be edited, searched and stored
 on a computer.
 
 When documents (especially pretty old ones written with a 
-typewriter), are digitised character recognition softwares often make mistakes.
+typewriter), are digitised character recognition softwares 
+often make mistakes.
 */
