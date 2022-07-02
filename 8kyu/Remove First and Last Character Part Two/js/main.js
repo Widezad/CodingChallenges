f89ce4a6f48170c419bfe19a5bbe56a2dffc97fa
widezad <Widezad@gmail.com>
2022-07-01 22:47:19 -0400

    New Coding Challenge

--- 8kyu/Remove First and Last Character Part Two/js/main.js
+++ 8kyu/Remove First and Last Character Part Two/js/main.js
@@ -8,4 +8,6 @@ time separated by spaces.
 If the input string is empty or the removal of the first and last 
 items would cause the resulting string to be empty, return an empty 
 value (represented as a generic value NULL in the examples below).
+
+Examples
 */
