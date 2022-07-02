7bc309c94dfb87887060e8259d9851d66c88d5bc
widezad <Widezad@gmail.com>
2022-07-01 23:01:30 -0400

    New Coding Challenge

--- 8kyu/Remove First and Last Character Part Two/js/main.js
+++ 8kyu/Remove First and Last Character Part Two/js/main.js
@@ -29,3 +29,5 @@ console.log(array('1,2,3')) //'2'
 console.log(array('1,2,3,4'))//'2 3'
 console.log(array('1,2,3,4,5'))//'2 3 4'
 console.log(array(''))//null
+console.log(array('1')) //null
+console.log(array('1,2')) //null
