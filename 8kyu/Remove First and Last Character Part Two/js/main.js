870723ded5ba90ae4e8fbd92bff94a95fe4c719a
widezad <Widezad@gmail.com>
2022-07-01 22:46:52 -0400

    New Coding Challenge

--- 8kyu/Remove First and Last Character Part Two/js/main.js
+++ 8kyu/Remove First and Last Character Part Two/js/main.js
@@ -6,5 +6,6 @@ Write a function which returns a new string containing the same
 character sequences except the first and the last ones but this 
 time separated by spaces.
 If the input string is empty or the removal of the first and last 
-items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+items would cause the resulting string to be empty, return an empty 
+value (represented as a generic value NULL in the examples below).
 */
