64144f056148c55a05f9cfa710b3ac1955e36a2c
widezad <Widezad@gmail.com>
2022-07-01 22:47:39 -0400

    New Coding Challenge

--- 8kyu/Remove First and Last Character Part Two/js/main.js
+++ 8kyu/Remove First and Last Character Part Two/js/main.js
@@ -10,4 +10,11 @@ items would cause the resulting string to be empty, return an empty
 value (represented as a generic value NULL in the examples below).
 
 Examples
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
 */
