82f762f6b3eb212c4857ba1ea2bb89b5c714bf11
widezad <Widezad@gmail.com>
2022-07-01 22:46:00 -0400

    New Coding Challenge

--- 8kyu/Remove First and Last Character Part Two/js/main.js
+++ 8kyu/Remove First and Last Character Part Two/js/main.js
@@ -2,6 +2,7 @@
 This is a spin off of my first kata.
 You are given a string containing a sequence of character sequences
 separated by commas.
-Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+Write a function which returns a new string containing the same 
+character sequences except the first and the last ones but this time separated by spaces.
 
 */
