d556fc8ff6610881eb11430059dcfba7014f429e
widezad <Widezad@gmail.com>
2022-07-02 23:23:56 -0400

    New Challenge

--- 8kyu/L1: Set Alarm/js/main.js
+++ 8kyu/L1: Set Alarm/js/main.js
@@ -1,4 +1,5 @@
 /*
 Write a function named setAlarm which receives two parameters. 
-The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+The first parameter, employed, is true whenever you are employed 
+and the second parameter, vacation is true whenever you are on vacation.
 */
