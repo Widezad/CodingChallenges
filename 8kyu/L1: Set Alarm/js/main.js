ff3ce0812a56efe0673ee51789199212aae0f969
widezad <Widezad@gmail.com>
2022-07-02 23:31:16 -0400

    New Challenge

--- 8kyu/L1: Set Alarm/js/main.js
+++ 8kyu/L1: Set Alarm/js/main.js
@@ -23,3 +23,4 @@ function setAlarm(employed, vacation){
 //test cases
 console.log(setAlarm(true, true)) //false
 console.log(setAlarm(false, true)) //false
+console.log(setAlarm(true, false)) //true
