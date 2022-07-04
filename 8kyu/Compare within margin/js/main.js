a9d8f32a0a347629e744fd502ac6aecabfbdb0c2
widezad <Widezad@gmail.com>
2022-07-03 21:25:00 -0400

    New Challenge

--- 8kyu/Compare within margin/js/main.js
+++ 8kyu/Compare within margin/js/main.js
@@ -39,4 +39,5 @@ function closeCompare(a, b, margin){
 console.log(closeCompare(4,5)) //-1
 console.log(closeCompare(5,5)) //0
 console.log(closeCompare(6,5)) //1
-console.log(closeCompare(-6, -5)) -1
+console.log(closeCompare(-6, -5)) //-1
+console.log(closeCompare(2,5,3)) //0
