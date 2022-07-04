cb07b0961a6c4bd5fe67b51135fc799aa40b3a84
widezad <Widezad@gmail.com>
2022-07-03 20:44:42 -0400

    New Challenge

--- 8kyu/Compare within margin/js/main.js
+++ 8kyu/Compare within margin/js/main.js
@@ -1,3 +1,4 @@
 /*
-Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
+Create a function close_compare that accepts 3 parameters: a, b, 
+and an optional margin. The function should return whether a is lower than, close to, or higher than b.
 */
