e7f389ed44590d6366f4f644b550ea86bf494807
widezad <Widezad@gmail.com>
2022-07-03 21:26:24 -0400

    New Challenge

--- 8kyu/Compare within margin/js/main.js
+++ 8kyu/Compare within margin/js/main.js
@@ -42,3 +42,4 @@ console.log(closeCompare(6,5)) //1
 console.log(closeCompare(-6, -5)) //-1
 console.log(closeCompare(2,5,3)) //0
 console.log(closeCompare(8.1, 5,3)) //1
+console.log(closeCompare(1.99,5,3)) //-1
