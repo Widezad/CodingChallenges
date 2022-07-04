8831833673f41bf0f2e87b41b7184a83b895a0e4
widezad <Widezad@gmail.com>
2022-07-03 21:09:42 -0400

    New Challenge

--- 8kyu/Compare within margin/js/main.js
+++ 8kyu/Compare within margin/js/main.js
@@ -30,3 +30,5 @@ If a = 3, b = 5, and margin = 0, then close_compare(a, b, margin)
 should return -1.
 This is because the distance between a and b is greater than 0, and a is less than b.
 */
+
+//write out function
