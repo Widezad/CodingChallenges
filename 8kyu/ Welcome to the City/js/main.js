92614765662f0eb230036d0339b597df8ade55ba
widezad <Widezad@gmail.com>
2022-07-04 21:49:50 -0400

    New Challenge

--- 8kyu/ Welcome to the City/js/main.js
+++ 8kyu/ Welcome to the City/js/main.js
@@ -19,4 +19,6 @@ function sayHello( name, city, state ) {
 //test cases
 console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'))
 //'Hello, John Smith! Welcome to Phoenix, Arizona!'
+console.log(sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois'))
+//'Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!'
 
