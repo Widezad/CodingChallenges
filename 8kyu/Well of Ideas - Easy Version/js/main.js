dbc86f8ceb0daca8f9a9f14d60aa322521cc55c7
widezad <Widezad@gmail.com>
2022-07-06 19:04:18 -0400

    New coding challenge

--- 8kyu/Well of Ideas - Easy Version/js/main.js
+++ 8kyu/Well of Ideas - Easy Version/js/main.js
@@ -6,3 +6,5 @@ In this kata you need to check the provided array (x) for good ideas
 'Publish!', if there are more than 2 return 'I smell a series!'. 
 If there are no good ideas, as is often the case, return 'Fail!'.
 */
+
+//write out function
