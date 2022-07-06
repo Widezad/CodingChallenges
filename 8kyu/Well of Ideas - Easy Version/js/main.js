27aa429ea2c08001284ffafadb423c2d65c4f56a
widezad <Widezad@gmail.com>
2022-07-06 19:21:51 -0400

    New coding challenge

--- 8kyu/Well of Ideas - Easy Version/js/main.js
+++ 8kyu/Well of Ideas - Easy Version/js/main.js
@@ -10,6 +10,12 @@ If there are no good ideas, as is often the case, return 'Fail!'.
 //write out function
 function well(x) {
     //conditional
+     if(x.includes('good')) {
+         return 'publish'
+    }
+    else if(x.includes(!'good')) {
+        return 'fail'
+    }
 }
 
 //test cases
