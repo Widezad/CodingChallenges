09ba7c3388f468bca033124115223a37380fda25
widezad <Widezad@gmail.com>
2022-07-05 21:24:07 -0400

    New coding challenge

--- 8kyu/Is this my tail?/js/main.js
+++ 8kyu/Is this my tail?/js/main.js
@@ -4,4 +4,6 @@ that perhaps the animals do not have the right tails. To help her,
 you must correct the broken function to make sure that the second 
 argument (tail), is the same as the last letter of the first argument 
 (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
 */
