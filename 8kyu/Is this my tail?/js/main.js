ef6a84d29d72424d478d749092b8267f3572e6d4
widezad <Widezad@gmail.com>
2022-07-05 21:24:51 -0400

    New coding challenge

--- 8kyu/Is this my tail?/js/main.js
+++ 8kyu/Is this my tail?/js/main.js
@@ -6,4 +6,6 @@ argument (tail), is the same as the last letter of the first argument
 (body) - otherwise the tail wouldn't fit!
 
 If the tail is right return true, else return false.
+
+The arguments will always be strings, and normal letters.
 */
