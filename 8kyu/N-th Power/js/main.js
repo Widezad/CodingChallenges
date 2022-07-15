183e0c89cf27f52a415c11d4d7fb932acd5072b3
widezad <Widezad@gmail.com>
2022-07-14 22:15:16 -0400

    New Challenge

--- 8kyu/N-th Power/js/main.js
+++ 8kyu/N-th Power/js/main.js
@@ -15,4 +15,9 @@ result is -1.
 
 function index(array, n){
     //your code here
+    if (array.length <= n || 0 > n) {
+      return -1;
+    } else {
+      return Math.pow(array[n], n);
+    }
 }
