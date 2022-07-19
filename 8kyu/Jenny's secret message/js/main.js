4d379c33e76a584e329183eab35f0f51a7bb32f0
widezad <Widezad@gmail.com>
2022-07-19 15:20:59 -0400

    New Challenge

--- 8kyu/Jenny's secret message/js/main.js
+++ 8kyu/Jenny's secret message/js/main.js
@@ -23,4 +23,6 @@ function greet(name){
     if(name === "Johnny")
       return "Hello, my love!";
 }
-console.log(greet("Jim")) //"Hello Jim!"
+console.log(greet("Jim")) //"Hello, Jim!"
+console.log(greet("Jane"))//"Hello, Jane!"
+console.log(greet("Simon"))//"Hello, Simon!""
