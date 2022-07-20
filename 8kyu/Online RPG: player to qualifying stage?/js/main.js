b337024a9f80eee7cf6828561fe949e719746004
widezad <Widezad@gmail.com>
2022-07-20 12:03:10 -0400

    New Coding challenge

--- 8kyu/Online RPG: player to qualifying stage?/js/main.js
+++ 8kyu/Online RPG: player to qualifying stage?/js/main.js
@@ -24,4 +24,5 @@ function playerRankUp(points) {
 }
 //test cases
 console.log(playerRankUp(64)) //false
-console.log(playerRankUp(101)) //"Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+console.log(playerRankUp(101)) //"Well done! You have advanced to the 
+//qualifying stage. Win 2 out of your next 3 games to rank up."
