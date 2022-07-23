c2172b3d95e6fed4c3743fb6d406329c4b55bd55
widezad <Widezad@gmail.com>
2022-07-22 20:11:30 -0400

    New Coding Challenge

--- 8kyu/Wilson primes/js/main.js
+++ 8kyu/Wilson primes/js/main.js
@@ -13,7 +13,16 @@ number is a Wilson prime.
 
 //function
 function amIWilson(p) {
-
+    const factorial = (n)=>{
+      if (n < 0) return -1
+      if (n === 0) return 1
+  
+      return n * factorial(n - 1)
+    }
+  
+    if( ((factorial(p-1) + 1) % (p * p)) == 0) return true
+  
+    return false
 }
 //test cases
 console.log(amIWilson(5)) //true
