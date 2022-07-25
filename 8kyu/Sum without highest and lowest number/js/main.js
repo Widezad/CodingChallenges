7dd34b2ed1cfc295e65688b1280413d94f597b2c
widezad <Widezad@gmail.com>
2022-07-25 10:30:13 -0400

    New Coding Challenge

--- 8kyu/Sum without highest and lowest number/js/main.js
+++ 8kyu/Sum without highest and lowest number/js/main.js
@@ -30,3 +30,4 @@ console.log(sumArray([ 3 ])) //0
 console.log(sumArray([ 3, 5 ])) //0
 console.log(sumArray([ 6, 2, 1, 8, 10 ])) //16
 console.log(sumArray([ 0, 1, 6, 10, 10 ])) //17
+console.log(sumArray([ -6, -20, -1, -10, -12 ])) //-28
