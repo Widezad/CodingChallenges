6c4e9391dabf114024b2c16f85fcb2fb707fdb8a
widezad <Widezad@gmail.com>
2022-07-25 10:30:13 -0400

    New Coding Challenge

--- 8kyu/Sum without highest and lowest number/js/main.js
+++ 8kyu/Sum without highest and lowest number/js/main.js
@@ -27,7 +27,7 @@ function sumArray(array) {
     else {
         array.sort((a,b) => a - b)
         let sum = 0
-        for(let i = 1; i <array.length - 1; i++) {
+        for(let i = 1; i < array.length - 1; i++) {
             sum+= array[i]
         }
         return sum
