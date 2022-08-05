eb661967f06fe40edbf5bed87b5631c2e1e72ec2
widezad <Widezad@gmail.com>
2022-08-05 11:30:21 -0400

    New Coding Challenge

--- 8kyu/Count by X/js/main.js
+++ 8kyu/Count by X/js/main.js
@@ -1,5 +1,6 @@
 /*
 Create a function with two arguments that will return an array of 
 the first (n) multiples of (x).
-Assume both the given number and the number of times to count will be positive numbers greater than 0.
+Assume both the given number and the number of times to count will
+be positive numbers greater than 0.
 */
