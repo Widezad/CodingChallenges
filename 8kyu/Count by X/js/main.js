a5cbd818d7609b39fa94518cac69be205d1c4b60
widezad <Widezad@gmail.com>
2022-08-07 10:20:14 -0400

    New Coding Challenge

--- 8kyu/Count by X/js/main.js
+++ 8kyu/Count by X/js/main.js
@@ -6,4 +6,7 @@ be positive numbers greater than 0.
 Return the results as an array (or list in Python, Haskell or Elixir).
 
 Examples:
+
+countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+countBy(2,5) === [2,4,6,8,10]
 */
