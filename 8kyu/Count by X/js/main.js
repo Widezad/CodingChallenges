6c230d6ec84accb6877f8b1ba2b35fc234275788
widezad <Widezad@gmail.com>
2022-08-08 09:36:08 -0400

    New Coding Challenge

--- 8kyu/Count by X/js/main.js
+++ 8kyu/Count by X/js/main.js
@@ -14,9 +14,10 @@ countBy(2,5) === [2,4,6,8,10]
 //function
 function countBy(x, n) {
     let z = [];
-    if(z.includes(x && n)) {
-        return z
+    for(let i = 1; i <= n; i++) {
+        z.push(x * i)
     }
+    return z
 }
 //test cases
 console.log(countBy(1,10)), //[1,2,3,4,5,6,7,8,9,10] //"Array does not match"
