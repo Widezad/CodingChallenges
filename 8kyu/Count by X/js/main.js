fd30b329706822ef5aef91dc4f85d311542ba625
widezad <Widezad@gmail.com>
2022-08-08 09:34:35 -0400

    New Coding Challenge

--- 8kyu/Count by X/js/main.js
+++ 8kyu/Count by X/js/main.js
@@ -19,5 +19,5 @@ function countBy(x, n) {
     }
 }
 //test cases
-console.log(countBy(1,10), [1,2,3,4,5,6,7,8,9,10]) //"Array does not match"
-console.log(countBy(2,5) [2,4,6,8,10]) //"Array does not match"
+console.log(countBy(1,10)), //[1,2,3,4,5,6,7,8,9,10] //"Array does not match"
+console.log(countBy(2,5)) //[2,4,6,8,10] //"Array does not match"
