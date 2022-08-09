d4c17504c6c0ab3365636a5ed48b215eb2ea32d6
widezad <Widezad@gmail.com>
2022-08-09 15:43:35 -0400

    New Challenge

--- 8kyu/Take the Derivative/js/main.js
+++ 8kyu/Take the Derivative/js/main.js
@@ -17,4 +17,9 @@ being the exponent minus 1.
 
 derive(7, 8) --> this should output "56x^7" 
 derive(5, 9) --> this should output "45x^8" 
+
+Notes:
+
+The output of this function should be a string
+The exponent will never be 1, and neither number will ever be 0
 */
