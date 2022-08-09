edf6fdb3d93d4f3edaa71c1dcbef93b67e30d7f2
widezad <Widezad@gmail.com>
2022-08-09 15:42:03 -0400

    New Challenge

--- 8kyu/Take the Derivative/js/main.js
+++ 8kyu/Take the Derivative/js/main.js
@@ -12,5 +12,6 @@ derive(7, 8)
 
 In this case, the function should multiply 7 and 8, and then 
 subtract 1 from 8. It should output "56x^7", the first number 
-56 being the product of the two numbers, and the second number being the exponent minus 1.
+56 being the product of the two numbers, and the second number 
+being the exponent minus 1.
 */
