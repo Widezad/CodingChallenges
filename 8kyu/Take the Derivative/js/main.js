3bbf894898f2e3b5e0365374fdaee30c3c22f6b7
widezad <Widezad@gmail.com>
2022-08-09 15:38:49 -0400

    New Challenge

--- 8kyu/Take the Derivative/js/main.js
+++ 8kyu/Take the Derivative/js/main.js
@@ -1,4 +1,5 @@
 /*
-This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+This function takes two numbers as parameters, the first number 
+being the coefficient, and the second number being the exponent.
 
 */
