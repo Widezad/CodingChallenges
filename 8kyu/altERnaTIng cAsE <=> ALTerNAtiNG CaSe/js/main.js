86f337b480c674df3c4d059958d5561cde2c949d
widezad <Widezad@gmail.com>
2022-08-12 16:43:55 -0400

    New Challenge

--- 8kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe/js/main.js
+++ 8kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe/js/main.js
@@ -18,3 +18,19 @@ characters are unaffected
 "String.prototype.toAlternatingCase".toAlternatingCase() ===
  "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
 */
+
+String.prototype.toAlternatingCase = function () {
+    // Define your method here :
+    const reg = /[A-Z]/;
+    let result = '';
+    
+    for(let i = 0; i < this.length; i += 1) {
+      if(reg.test(this[i])) {
+        result += this[i].toLowerCase();
+      } else {
+        result += this[i].toUpperCase();
+      }
+    }
+    
+    return result
+}
