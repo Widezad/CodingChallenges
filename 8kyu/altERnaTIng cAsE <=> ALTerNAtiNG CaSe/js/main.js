e97840961a4b8a519e7943f06415f5a7537731f4
widezad <Widezad@gmail.com>
2022-08-12 16:41:37 -0400

    New Challenge

--- 8kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe/js/main.js
+++ 8kyu/altERnaTIng cAsE <=> ALTerNAtiNG CaSe/js/main.js
@@ -12,7 +12,9 @@ uppercase and each uppercase letter becomes lowercase. For example:
 "HELLO WORLD".toAlternatingCase() === "hello world"
 "hello WORLD".toAlternatingCase() === "HELLO world"
 "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
-"12345".toAlternatingCase() === "12345" // Non-alphabetical characters are unaffected
+"12345".toAlternatingCase() === "12345" // Non-alphabetical 
+characters are unaffected
 "1a2b3c4d5e".toAlternatingCase() === "1A2B3C4D5E"
-"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+"String.prototype.toAlternatingCase".toAlternatingCase() ===
+ "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
 */
