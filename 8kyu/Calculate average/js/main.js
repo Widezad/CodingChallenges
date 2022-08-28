31e3748ca2b0037f2d31860217de17f6c7d70056
widezad <Widezad@gmail.com>
2022-08-28 01:30:14 -0400

    New Challenge

--- 8kyu/Calculate average/js/main.js
+++ 8kyu/Calculate average/js/main.js
@@ -14,5 +14,5 @@ function findAverage(array) {
 
 //Console
 console.log(findAverage([1,1,1])) //1
-console/log(findAverage([1,2,3])) //2
+console.log(findAverage([1,2,3])) //2
 console.log(findAverage([1,2,3,4])) //2.5
