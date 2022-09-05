85562b4d88540f856a074a4e8bb8e21e91a1250a
widezad <Widezad@gmail.com>
2022-09-05 10:30:10 -0400

    New Challenge

--- 8kyu/Calculate average/js/main.js
+++ 8kyu/Calculate average/js/main.js
@@ -5,7 +5,7 @@ Note: Empty array should return 0
 
 //Write function
 function findAverage(array) {
-    if(array === null || array == NaN || array === undefined) {
+    if(array === null || array == NaN || array === undefined || array === 0) {
         return 0
     }else {
         return array.reduce((a,b) => a + b) / array.length
