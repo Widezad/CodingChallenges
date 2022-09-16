d64d3962ccf964014fa070ffe97514a24ad255b3
widezad <Widezad@gmail.com>
2022-09-16 18:11:48 -0400

    New Challenge

--- 8kyu/Training JS #2: Basic data types--Number/js/main.js
+++ 8kyu/Training JS #2: Basic data types--Number/js/main.js
@@ -1,3 +1,4 @@
 /*
-In javascript, Number is one of basic data types. It can be positive: 1,2,3, negative:-1,-100 , integer:123,456, decimal:3.1415926,-8.88 etc..
+In javascript, Number is one of basic data types. 
+It can be positive: 1,2,3, negative:-1,-100 , integer:123,456, decimal:3.1415926,-8.88 etc..
 */
