c7f0865eb0041444ce37b476480596684011a4a5
widezad <Widezad@gmail.com>
2022-09-20 18:51:23 -0400

    New Challenge

--- 8kyu/Training JS #2: Basic data types--Number/js/main.js
+++ 8kyu/Training JS #2: Basic data types--Number/js/main.js
@@ -43,8 +43,8 @@ function equal2(){
 }
   
   function equal4(){
-    let a =  , //set number value to a
-        b =  ; //set number value to b
+    let a =  v4, //set number value to a
+        b =  v4; //set number value to b
     return a / b;
 }
   
