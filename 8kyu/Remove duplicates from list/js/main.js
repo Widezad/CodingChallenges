503792b153916cc010feacb73b2763881af52166
widezad <Widezad@gmail.com>
2022-09-21 10:52:42 -0400

    New Challenge

--- 8kyu/Remove duplicates from list/js/main.js
+++ 8kyu/Remove duplicates from list/js/main.js
@@ -11,14 +11,8 @@ THe order of the sequence has to stay the same
 //Define the function to get unique values from an array
 function distinct(a) {
     let arr = []
-
-    //Loop through the array values
-    for(let i = 0; i < a.length; i++) {
-        if(arr.indexOf(arr[i] === -1)) {
-            arr.push(arr[i])
-        }
-    }
-    return arr
+    return arr = [...new Set(a)]
+   
 }
 
 //Test cases 
