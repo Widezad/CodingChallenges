0d9614286ea024f9fc7f846f232d2a35147501e2
widezad <Widezad@gmail.com>
2022-09-22 20:46:26 -0400

    New Challenge

--- 8kyu/The Feast of Many Beasts/js/main.js
+++ 8kyu/The Feast of Many Beasts/js/main.js
@@ -18,7 +18,7 @@ or end of the string. They will not contain numerals.
 
 //Function
 function feast(beast, dish) {
-    if(beast.includes(dish.charAt[0])) {
+    if(beast.includes(dish[0])) {
         return true
     }
     else {
