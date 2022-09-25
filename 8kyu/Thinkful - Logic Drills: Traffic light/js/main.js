f08f0d3dcf178fcd1358349f13957abbbc5e5112
widezad <Widezad@gmail.com>
2022-09-24 23:33:48 -0400

    New Challenge

--- 8kyu/Thinkful - Logic Drills: Traffic light/js/main.js
+++ 8kyu/Thinkful - Logic Drills: Traffic light/js/main.js
@@ -4,5 +4,6 @@ You need a function to handle each change from green,
 to yellow, to red, and then to green again.
 
 Complete the function that takes a string as an argument 
-representing the current state of the light and returns a string representing the state the light should change to.
+representing the current state of the light and returns a 
+string representing the state the light should change to.
 */
