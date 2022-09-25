2f713df64bd9a60f90635a2640d7f3142dcfec12
widezad <Widezad@gmail.com>
2022-09-24 23:32:55 -0400

    New Challenge

--- 8kyu/Thinkful - Logic Drills: Traffic light/js/main.js
+++ 8kyu/Thinkful - Logic Drills: Traffic light/js/main.js
@@ -1,3 +1,4 @@
 /*
-You're writing code to control your town's traffic lights.You need a function to handle each change from green, to yellow, to red, and then to green again.
+You're writing code to control your town's traffic lights.
+You need a function to handle each change from green, to yellow, to red, and then to green again.
 */
