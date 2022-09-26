065e84d164e98526a9fb9ce8918b0e166148727a
widezad <Widezad@gmail.com>
2022-09-26 11:35:46 -0400

    New Challenge

--- 8kyu/Simple Fun #1: Seats in Theater/js/main.js
+++ 8kyu/Simple Fun #1: Seats in Theater/js/main.js
@@ -11,5 +11,6 @@ out, which is quite simple, especially since the exit is
 located right behind your row to the left. All you need to do 
 is climb over your seat and make your way to the exit.
 
-The main problem is your shyness: you're afraid that you'll end up blocking the view (even if only for a couple of seconds) of all the people who sit behind you and in your column or the columns to your left. To gain some courage, you decide to calculate the number of such people and see if you can possibly make it to the exit without disturbing too many people.
+The main problem is your shyness: you're afraid that you'll end 
+up blocking the view (even if only for a couple of seconds) of all the people who sit behind you and in your column or the columns to your left. To gain some courage, you decide to calculate the number of such people and see if you can possibly make it to the exit without disturbing too many people.
 */
