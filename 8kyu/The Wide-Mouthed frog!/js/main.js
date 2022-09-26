9846744c38945ee7a6138fb4ef791f45cb6b21f2
widezad <Widezad@gmail.com>
2022-09-25 20:20:21 -0400

    New Challenge

--- 8kyu/The Wide-Mouthed frog!/js/main.js
+++ 8kyu/The Wide-Mouthed frog!/js/main.js
@@ -15,6 +15,4 @@ mouth_size method this method takes one argument animal which
 corresponds to the animal encountered by the frog. If this one 
 is an alligator (case-insensitive) return small otherwise return
 wide
-
-
 */
