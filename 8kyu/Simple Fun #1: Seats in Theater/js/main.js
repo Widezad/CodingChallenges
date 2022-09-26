6c7e23d08b28373d51a61c2084f28df53f189187
widezad <Widezad@gmail.com>
2022-09-26 11:42:17 -0400

    New Challenge

--- 8kyu/Simple Fun #1: Seats in Theater/js/main.js
+++ 8kyu/Simple Fun #1: Seats in Theater/js/main.js
@@ -74,3 +74,4 @@ function seatsInTheater(nCols, nRows, col, row) {
 //Test cases
 console.log(seatsInTheater(16,11,5,3)) //96
 console.log(seatsInTheater(1,1,1,)) //0
+console.log(seatsInTheater(13,6,8,3)) //18
