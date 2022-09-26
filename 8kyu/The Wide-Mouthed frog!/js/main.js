ff077589c52bad5f5106bcfc12e3e5a6cf939daa
widezad <Widezad@gmail.com>
2022-09-25 20:18:43 -0400

    New Challenge

--- 8kyu/The Wide-Mouthed frog!/js/main.js
+++ 8kyu/The Wide-Mouthed frog!/js/main.js
@@ -3,4 +3,9 @@ DESCRIPTION:
 
 The wide-mouth frog is particularly interested in the eating 
 habits of other creatures.
+
+He just can't stop asking the creatures he encounters what they 
+like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+
 */
