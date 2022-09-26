94095d0732c9ca2d29f5acf27c8622cc9463b7bf
widezad <Widezad@gmail.com>
2022-09-26 11:37:00 -0400

    New Challenge

--- 8kyu/Simple Fun #1: Seats in Theater/js/main.js
+++ 8kyu/Simple Fun #1: Seats in Theater/js/main.js
@@ -18,5 +18,6 @@ columns to your left. To gain some courage, you decide to
 calculate the number of such people and see if you can possibly 
 make it to the exit without disturbing too many people.
 
-Given the total number of rows and columns in the theater (nRows and nCols, respectively), and the row and column you're sitting in, return the number of people who sit strictly behind you and in your column or to the left, assuming all seats are occupied.
+Given the total number of rows and columns in the theater 
+(nRows and nCols, respectively), and the row and column you're sitting in, return the number of people who sit strictly behind you and in your column or to the left, assuming all seats are occupied.
 */
