c8c871d42f59fff29bf6ad4f73b5c8164a0858ea
widezad <Widezad@gmail.com>
2022-09-27 19:25:56 -0400

    New Challenge

--- 8kyu/Is n divisible by x and y?/js/main.js
+++ 8kyu/Is n divisible by x and y?/js/main.js
@@ -11,6 +11,7 @@ Examples:
 2 and 6
 3) n = 100, x = 5, y = 3 => false because 100 is not divisible 
 by 3
-4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+4) n =  12, x = 7, y = 5 => false because  12 is neither 
+divisible by 7 nor 5
 
 */
