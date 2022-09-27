d780bcc674d6f47664785de985dfd781cdc191aa
widezad <Widezad@gmail.com>
2022-09-27 19:22:58 -0400

    New Challenge

--- 8kyu/Is n divisible by x and y?/js/main.js
+++ 8kyu/Is n divisible by x and y?/js/main.js
@@ -1,5 +1,6 @@
 /*
 DESCRIPTION:
 
-Create a function that checks if a number n is divisible by two numers x AND y. All inputs are positive, non-Zero numbers.
+Create a function that checks if a number n is divisible by two 
+numers x AND y. All inputs are positive, non-Zero numbers.
 */
