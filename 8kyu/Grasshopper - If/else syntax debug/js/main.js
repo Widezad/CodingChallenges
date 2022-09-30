ee04087bf76373280e817c5540280aeda3ee6f5d
widezad <Widezad@gmail.com>
2022-09-29 22:00:24 -0400

    New Challenge

--- 8kyu/Grasshopper - If/else syntax debug/js/main.js
+++ 8kyu/Grasshopper - If/else syntax debug/js/main.js
@@ -8,6 +8,7 @@ function to check if the user is still alive called
 checkAlive/CheckAlive/check_alive. Unfortunately, Greg made 
 some errors while creating the function.
 
-checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+checkAlive/CheckAlive/check_alive should return true if the 
+player's health is greater than 0 or false if it is 0 or below.
 
 */
