9aa8262214c22a1b69dbf0d3a7f69e1658268251
widezad <Widezad@gmail.com>
2022-09-29 21:59:06 -0400

    New Challenge

--- 8kyu/Grasshopper - If/else syntax debug/js/main.js
+++ 8kyu/Grasshopper - If/else syntax debug/js/main.js
@@ -3,5 +3,6 @@ DESCRIPTION:
 
 If/else syntax debug
 
-While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+While making a game, your partner, Greg, decided to create a 
+function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
 */
