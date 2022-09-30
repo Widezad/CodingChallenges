8935ad7ef4b1e5a9f27253be6fa9024ef977a675
widezad <Widezad@gmail.com>
2022-09-29 22:02:10 -0400

    New Challenge

--- 8kyu/Grasshopper - If/else syntax debug/js/main.js
+++ 8kyu/Grasshopper - If/else syntax debug/js/main.js
@@ -13,6 +13,6 @@ player's health is greater than 0 or false if it is 0 or below.
 
 The function receives one parameter health which will always be 
 a whole number between -10 and 10.
+*/
 
 //Function
-*/
