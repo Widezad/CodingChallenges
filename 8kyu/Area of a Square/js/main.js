c83237ff096cf29870ef47fd20d397411244e40b
widezad <Widezad@gmail.com>
2022-09-30 22:56:10 -0400

    New Challenge

--- 8kyu/Area of a Square/js/main.js
+++ 8kyu/Area of a Square/js/main.js
@@ -1,3 +1,5 @@
 /*
 DESCRIPTION:
+
+Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
 */
