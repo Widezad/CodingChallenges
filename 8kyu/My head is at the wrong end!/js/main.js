16ba9b2a69ee8fd56179d38ef1d59f8da2e5d9f4
widezad <Widezad@gmail.com>
2022-10-01 10:30:40 -0400

    New Challenge

--- 8kyu/My head is at the wrong end!/js/main.js
+++ 8kyu/My head is at the wrong end!/js/main.js
@@ -1,5 +1,6 @@
 /*
 DESCRIPTION:
 
-You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+You're at the zoo... all the meerkats look weird. Something has 
+gone terribly wrong - someone has gone and switched their heads and tails around!
 */
