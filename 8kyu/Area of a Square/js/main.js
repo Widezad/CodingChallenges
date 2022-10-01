25024304973117ba3e982fb2fa09da0941c04285
widezad <Widezad@gmail.com>
2022-09-30 22:57:41 -0400

    New Challenge

--- 8kyu/Area of a Square/js/main.js
+++ 8kyu/Area of a Square/js/main.js
@@ -6,4 +6,5 @@ when the length of the circular arc A is given as the input.
 Return the result rounded to two decimals.
 
 Note:
+use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
 */
