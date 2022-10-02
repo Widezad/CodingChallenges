105dc41ae550e5a80af8c4c91cc3277e0d0522fa
widezad <Widezad@gmail.com>
2022-10-02 19:26:16 -0400

    New Challenge

--- 8kyu/My head is at the wrong end!/js/main.js
+++ 8kyu/My head is at the wrong end!/js/main.js
@@ -6,5 +6,6 @@ gone terribly wrong - someone has gone and switched their heads
 and tails around!
 
 Save the animals by switching them back. You will be given an 
-array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+array which will have three values (tail, body, head). It is
+your job to re-arrange the array so that the animal is the right way round (head, body, tail).
 */
