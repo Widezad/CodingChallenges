e06a46a4781ebdc7fbb4addf643edbec8c893484
widezad <Widezad@gmail.com>
2022-10-02 19:30:02 -0400

    New Challenge

--- 8kyu/My head is at the wrong end!/js/main.js
+++ 8kyu/My head is at the wrong end!/js/main.js
@@ -13,4 +13,6 @@ way round (head, body, tail).
 Same goes for all the other arrays/lists that you will get in 
 the tests: you have to change the element positions with the same 
 exact logics
+
+Simples!
 */
