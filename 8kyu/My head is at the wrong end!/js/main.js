514ebc6c5050ecac341805328407f6c2764b6684
widezad <Widezad@gmail.com>
2022-10-02 19:29:00 -0400

    New Challenge

--- 8kyu/My head is at the wrong end!/js/main.js
+++ 8kyu/My head is at the wrong end!/js/main.js
@@ -11,5 +11,6 @@ your job to re-arrange the array so that the animal is the right
 way round (head, body, tail).
 
 Same goes for all the other arrays/lists that you will get in 
-the tests: you have to change the element positions with the same exact logics
+the tests: you have to change the element positions with the same 
+exact logics
 */
