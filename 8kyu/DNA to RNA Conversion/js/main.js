cb502a712f9cdb3f2dad8d98b10a8b00fb9c094e
widezad <Widezad@gmail.com>
2022-10-03 22:53:10 -0400

    New Challenge

--- 8kyu/DNA to RNA Conversion/js/main.js
+++ 8kyu/DNA to RNA Conversion/js/main.js
@@ -7,5 +7,6 @@ acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and
 Thymine ('T').
 
 Ribonucleic acid, RNA, is the primary messenger molecule in 
-cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+cells. RNA differs slightly from DNA its chemical structure 
+and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
 */
