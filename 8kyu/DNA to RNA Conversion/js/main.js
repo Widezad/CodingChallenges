00e8880d42a003ebb170c0d7e3e0b82de5d7f79f
widezad <Widezad@gmail.com>
2022-10-03 22:51:44 -0400

    New Challenge

--- 8kyu/DNA to RNA Conversion/js/main.js
+++ 8kyu/DNA to RNA Conversion/js/main.js
@@ -1,3 +1,5 @@
 /*
 DESCRIPTION:
+
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 */
