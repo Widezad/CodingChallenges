1738c8b64910dbcff1e4699ba2f69feca41bdbb5
widezad <Widezad@gmail.com>
2022-10-04 18:48:13 -0400

    New Challenge

--- 8kyu/Switch/Case - Bug Fixing #6/js/main.js
+++ 8kyu/Switch/Case - Bug Fixing #6/js/main.js
@@ -7,5 +7,16 @@ can you fix timmy's function?
 */
 
 //FUnction
-
+function evalObject(value){
+    var result = 0;
+    switch(value.operation){
+      case'+': result = value.a + value.b;
+      case'-': result = value.a - value.b;
+      case'/': result = value.a / value.b;
+      case'*': result = value.a * value.b;
+      case'%': result = value.a % value.b;
+      case'^': result = Math.pow(value.a, value.b);
+    }
+    return result;
+}
 //Test Cases
