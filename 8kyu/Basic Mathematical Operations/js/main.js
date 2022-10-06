69e8d1bc7062eaf5afa3102e570fa1bfa290b857
widezad <Widezad@gmail.com>
2022-10-05 22:29:17 -0400

    New Challenge

--- 8kyu/Basic Mathematical Operations/js/main.js
+++ 8kyu/Basic Mathematical Operations/js/main.js
@@ -6,4 +6,6 @@ mathematical operations.
 
 The function should take three arguments - operation
 (string/char), value1(number), value2(number).
+
+The function should return result of numbers after applying the chosen operation.
 */
