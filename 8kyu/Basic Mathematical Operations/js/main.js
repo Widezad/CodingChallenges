418925ff98c91136aa287e1ff25ad005b102d304
widezad <Widezad@gmail.com>
2022-10-05 23:11:23 -0400

    New Challenge

--- 8kyu/Basic Mathematical Operations/js/main.js
+++ 8kyu/Basic Mathematical Operations/js/main.js
@@ -26,6 +26,9 @@ function basicOp(operation, value1, value2) {
     else if( operation === '-') {
         return value1 - value2
     }
+    else if( operation === '*') {
+        return value1 * value2
+    }
  
 }
 
