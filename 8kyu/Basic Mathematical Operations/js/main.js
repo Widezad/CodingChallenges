3e522047db5224922f23a7d24ed332ccb0ad2ea6
widezad <Widezad@gmail.com>
2022-10-05 23:06:24 -0400

    New Challenge

--- 8kyu/Basic Mathematical Operations/js/main.js
+++ 8kyu/Basic Mathematical Operations/js/main.js
@@ -27,4 +27,5 @@ function basicOp(operation, value1, value2) {
 console.log(basicOp('+', 4,7)) //11
 console.log(basicOp('-', 15, 18)) //-3
 console.log(basicOp('*', 5,5)) //25
+console.log(basicOp('/', 49))//7
 
