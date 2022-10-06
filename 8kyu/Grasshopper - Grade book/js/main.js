bd5f4a59bc5810dddca37114d730eccf52f29162
widezad <Widezad@gmail.com>
2022-10-06 16:06:37 -0400

    New Challenge

--- 8kyu/Grasshopper - Grade book/js/main.js
+++ 8kyu/Grasshopper - Grade book/js/main.js
@@ -20,20 +20,19 @@ check for negative values or values greater than 100.
 */
 
 //Fucntion
-function getGrade(s1, s2, s3) {
-    let score = (s1 + s2 + s3) /3
-    if (90 <= score && score >= 100) {
+function getGrade (s1, s2, s3) {
+    let score = (s1 + s2 + s3) / 3;
+    if (score >= 90 && score <= 100) {
         return 'A';
-    } else if (80 <= score && score > 90) {
+    } else if (score >= 80 && score < 90) {
       return 'B';
-    } else if (70 <= score && score > 80) {
+    } else if (score >= 70&& score < 80) {
       return 'C';
-    } else if (60 <= score && score > 70) {
+    } else if (score >= 60  && score < 70) {
       return 'D';
-    } else if (0 <= score && score > 60) {
+    } else {
       return 'F';
     }
-
 }
 //Test Cases
 console.log(getGrade(95, 90, 93)) // 'A'
