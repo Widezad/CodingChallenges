291116fc8aba152199295884caebe9617640f2a4
widezad <Widezad@gmail.com>
2022-10-05 22:28:38 -0400

    New Challenge

--- 8kyu/Basic Mathematical Operations/js/main.js
+++ 8kyu/Basic Mathematical Operations/js/main.js
@@ -3,4 +3,6 @@ DESCRIPTION:
 
 Your task is to create a function that does four basic 
 mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
 */
