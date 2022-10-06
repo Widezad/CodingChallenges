51b1a41bbc08fb45290d5f37e14ff48422e50715
widezad <Widezad@gmail.com>
2022-10-06 15:32:46 -0400

    New Challenge

--- 8kyu/Grasshopper - Grade book/js/main.js
+++ 8kyu/Grasshopper - Grade book/js/main.js
@@ -4,5 +4,6 @@ DESCRIPTION:
 GRADEBOOK
 
 Complete the function so that it finds the average of the three 
-scores passed to it and returns the letter value associated with that grade.
+scores passed to it and returns the letter value associated with 
+that grade.
 */
