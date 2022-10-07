5c113d5e4480309756cf91289e62490ef7ed2803
widezad <Widezad@gmail.com>
2022-10-07 13:34:45 -0400

    New Challenge

--- 8kyu/Multiple of index/js/main.js
+++ 8kyu/Multiple of index/js/main.js
@@ -15,7 +15,18 @@ Some Cases:
 
 //Function
 function multipleOfIndex(array) {
-    // good luck
+    let res = [];
+
+	// loop over the array
+	for (let i = 0; i < array.length; i++) {
+		// check if the current element (array[i]) is a multiple of it's index (i) and if it is...
+		if (array[i] % i === 0) {
+			// add it to the resulting array
+			res.push(array[i]);
+		}
+	}
+
+	return res;
 }
 //Test Cases
 console.log(multipleOfIndex([22, -6, 32, 82, 9, 25])) //[-6, 32, 25]
