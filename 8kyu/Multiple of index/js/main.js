f5399ae23a5b5b0f792384f6091768cd469c6d9e
widezad <Widezad@gmail.com>
2022-10-07 13:08:18 -0400

    New Challenge

--- 8kyu/Multiple of index/js/main.js
+++ 8kyu/Multiple of index/js/main.js
@@ -21,3 +21,4 @@ function multipleOfIndex(array) {
 console.log(multipleOfIndex([22, -6, 32, 82, 9, 25])) //[-6, 32, 25]
 console.log(multipleOfIndex([68, -1, 1, -7, 10, 10])) //[-1, 10]
 console.log(multipleOfIndex([11, -11])) //[-11]
+console.log(multipleOfIndex([-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68])) //[-85, 72, 0, 68]
