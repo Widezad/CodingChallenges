065ffca34439ab64280be383a4230985453217e4
widezad <Widezad@gmail.com>
2022-10-08 23:25:19 -0400

    New Challenge

--- 8kyu/Multiplication table for number/js/main.js
+++ 8kyu/Multiplication table for number/js/main.js
@@ -27,7 +27,11 @@ look at the sample tests.
 
 //Function
 function multiTable(number) {
-
+    const result
+    for(let i = 1; i <=10; i++) {
+        return result = i * number
+    }
+    return (`${number} * ${i} = ${result}`)
 }
 //Test Cases
 console.log(multiTable(5)) //'1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50'
