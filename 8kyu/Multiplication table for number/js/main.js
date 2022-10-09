e6a72cb9ecd4479b59d47540aa3ef5d56cdc1a4f
widezad <Widezad@gmail.com>
2022-10-08 23:13:34 -0400

    New Challenge

--- 8kyu/Multiplication table for number/js/main.js
+++ 8kyu/Multiplication table for number/js/main.js
@@ -1,5 +1,6 @@
 /*
 DESCRIPTION:
 
-Your goal is to return multiplication table for number that is always an integer from 1 to 10
+Your goal is to return multiplication table for number that is always 
+an integer from 1 to 10
 */
