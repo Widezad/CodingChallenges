70a3804d8b1f3f24ef4f3c8e2d6c2765d3e67c82
widezad <Widezad@gmail.com>
2022-10-08 23:16:14 -0400

    New Challenge

--- 8kyu/Multiplication table for number/js/main.js
+++ 8kyu/Multiplication table for number/js/main.js
@@ -20,5 +20,6 @@ like below:
 
 P. S. You can use \n in string to jump to the next line.
 
-Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
+Note: newlines should be added between rows, but there should be no 
+trailing newline at the end. If you're unsure about the format, look at the sample tests.
 */
