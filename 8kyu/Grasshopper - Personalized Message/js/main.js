c809b2d5e00c007db3a9502ad87e9d5f873904e2
widezad <Widezad@gmail.com>
2022-10-11 21:03:36 -0400

    New Challenge

--- 8kyu/Grasshopper - Personalized Message/js/main.js
+++ 8kyu/Grasshopper - Personalized Message/js/main.js
@@ -5,4 +5,6 @@ Create a function that gives a personalized greeting. This function takes
 two parameters: name and owner.
 
 Use conditionals to return the proper message:
+
+case                                    return
 */
