1c42baff95bbca7ff9ed54274693807b41da7805
widezad <Widezad@gmail.com>
2022-10-11 21:02:36 -0400

    New Challenge

--- 8kyu/Grasshopper - Personalized Message/js/main.js
+++ 8kyu/Grasshopper - Personalized Message/js/main.js
@@ -1,5 +1,6 @@
 /*
 DESCRIPTION:
 
-Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+Create a function that gives a personalized greeting. This function takes
+two parameters: name and owner.
 */
