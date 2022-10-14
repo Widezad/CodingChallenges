5a79c381360addbc44cea0aa2e0d6697595ba78a
widezad <Widezad@gmail.com>
2022-10-13 21:09:31 -0400

    New Challenge

--- 8kyu/Transportation on vacation/js/main.js
+++ 8kyu/Transportation on vacation/js/main.js
@@ -7,4 +7,6 @@ try to leave all the mess behind you
 
 You will need a rental car in order for you to get around in your 
 vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 */
