953c7f96526be47266ee4e49a2825485e217b76a
widezad <Widezad@gmail.com>
2022-10-13 21:06:36 -0400

    New Challenge

--- 8kyu/Transportation on vacation/js/main.js
+++ 8kyu/Transportation on vacation/js/main.js
@@ -1,5 +1,6 @@
 /*
 DESCRIPTION:
 
-After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you
+After a hard quarter in the office you decide to get some rest on a 
+vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you
 */
