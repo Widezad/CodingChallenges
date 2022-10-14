eb170c9c3784e091038380ed86905488e932ce89
widezad <Widezad@gmail.com>
2022-10-13 21:25:54 -0400

    New Challenge

--- 8kyu/Transportation on vacation/js/main.js
+++ 8kyu/Transportation on vacation/js/main.js
@@ -25,3 +25,4 @@ console.log(rentalCarCost(2)) //80
 console.log(rentalCarCost(3)) //100
 console.log(rentalCarCost(4)) //140
 console.log(rentalCarCost(5)) //180
+console.log(rentalCarCost(6)) //220
