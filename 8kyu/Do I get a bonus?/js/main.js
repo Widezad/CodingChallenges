e4f4f5e36275d063adad0568d4e9cf1f7650fef1
widezad <Widezad@gmail.com>
2022-10-14 22:09:08 -0400

    New Challenge

--- 8kyu/Do I get a bonus?/js/main.js
+++ 8kyu/Do I get a bonus?/js/main.js
@@ -3,4 +3,8 @@ DESCRIPTION:
 
 It's bonus time in the big city! The fatcats are rubbing their paws 
 in anticipation... but who is going to make the most money?
+
+Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+
 */
