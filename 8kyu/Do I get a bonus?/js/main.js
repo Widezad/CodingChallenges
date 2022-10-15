6783bf2ae7c26e69d71d832c8748e27099293592
widezad <Widezad@gmail.com>
2022-10-14 22:10:31 -0400

    New Challenge

--- 8kyu/Do I get a bonus?/js/main.js
+++ 8kyu/Do I get a bonus?/js/main.js
@@ -7,7 +7,8 @@ in anticipation... but who is going to make the most money?
 Build a function that takes in two arguments (salary, bonus). 
 Salary will be an integer, and bonus a boolean.
 
-If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+If bonus is true, the salary should be multiplied by 10. If bonus 
+is false, the fatcat did not make enough money and must receive only his stated salary.
 
 
 */
