eb87a77339dc923b42c6713d92e9f43b748108ee
widezad <Widezad@gmail.com>
2022-10-14 22:08:27 -0400

    New Challenge

--- 8kyu/Do I get a bonus?/js/main.js
+++ 8kyu/Do I get a bonus?/js/main.js
@@ -1,5 +1,6 @@
 /*
 DESCRIPTION:
 
-It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+It's bonus time in the big city! The fatcats are rubbing their paws 
+in anticipation... but who is going to make the most money?
 */
