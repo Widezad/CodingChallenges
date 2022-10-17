0fe97c1528c11f7fa6ed67143d345b76ac553fd2
widezad <Widezad@gmail.com>
2022-10-16 22:01:00 -0400

    New Challenge

--- 8kyu/Grasshopper - Check for factor/js/main.js
+++ 8kyu/Grasshopper - Check for factor/js/main.js
@@ -6,4 +6,6 @@ This function should test if the factor is a factor of a base.
 Return true if it is a factor ir false if it is not
 
 About Factors
+
+Factors are numbers you  can multiply together to get another number
 */
