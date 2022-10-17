20dccaf7e2db5d8457aa2ba19eb4154c14999be1
widezad <Widezad@gmail.com>
2022-10-16 22:05:17 -0400

    New Challenge

--- 8kyu/Grasshopper - Check for factor/js/main.js
+++ 8kyu/Grasshopper - Check for factor/js/main.js
@@ -24,6 +24,7 @@ Note: base is a non-negative number, factor is a positive number.
 
 //Fucntion
 function checkForFactor(base, factor) {
-    
+
 }
 //Test Cases
+console.log(checkForFactor(10,2)) //true
