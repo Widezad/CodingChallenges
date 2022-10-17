a7d91ad9127d6aae50d4fdf105661b2d917e9521
widezad <Widezad@gmail.com>
2022-10-16 22:01:58 -0400

    New Challenge

--- 8kyu/Grasshopper - Check for factor/js/main.js
+++ 8kyu/Grasshopper - Check for factor/js/main.js
@@ -10,4 +10,6 @@ About Factors
 Factors are numbers you  can multiply together to get another number
 
 2 and 3 are factors of 6 because 2 * 3 = 6
+
+You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
 */
