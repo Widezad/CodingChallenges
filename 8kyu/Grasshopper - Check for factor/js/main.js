b74c80707962f63bbe5e89505e6baf48f690fb65
widezad <Widezad@gmail.com>
2022-10-16 22:04:04 -0400

    New Challenge

--- 8kyu/Grasshopper - Check for factor/js/main.js
+++ 8kyu/Grasshopper - Check for factor/js/main.js
@@ -18,4 +18,6 @@ You can use the mod operator (%) in most languages to check for a
 remainder
 
 For example 2 is not a factor of 7 because 7 % 2 = 1
+
+Note: base is a non-negative number, factor is a positive number.
 */
