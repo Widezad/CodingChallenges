6061b9fed2c39ccaec7392548057dbbf32ba4a6b
widezad <Widezad@gmail.com>
2022-10-16 22:11:16 -0400

    New Challenge

--- 8kyu/Grasshopper - Check for factor/js/main.js
+++ 8kyu/Grasshopper - Check for factor/js/main.js
@@ -32,3 +32,4 @@ console.log(checkForFactor(653,7)) //false
 console.log(checkForFactor(2450,5)) //false
 console.log(checkForFactor(24612, 3)) //false
 console.log(checkForFactor(9,2)) //false
+console.log(checkForFactor(653, 7)) //false
