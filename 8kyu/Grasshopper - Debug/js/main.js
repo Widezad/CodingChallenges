54151e3145a89a12eb84ba91d6987169f19baf8d
widezad <Widezad@gmail.com>
2022-10-17 22:46:30 -0400

    New Challenge

--- 8kyu/Grasshopper - Debug/js/main.js
+++ 8kyu/Grasshopper - Debug/js/main.js
@@ -17,5 +17,6 @@ celsius = (farenheit - 32) * (5/9)
 Remember that typically temperatures in the current weather conditions 
 are given in whole numbers. It is possible for temperature sensors to 
 report temperatures with a higher accuracy such as to the nearest tenth. 
-Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
+Instrument error though makes this sort of accuracy unreliable for many 
+types of temperature measuring sensors.
 */
