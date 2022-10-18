b45afd6d104ec508e3927e6a1f89a39a98987483
widezad <Widezad@gmail.com>
2022-10-17 22:42:51 -0400

    New Challenge

--- 8kyu/Grasshopper - Debug/js/main.js
+++ 8kyu/Grasshopper - Debug/js/main.js
@@ -2,4 +2,6 @@
 DESCRITPTION:
 
 Debug celsius converter
+
+Your friend is traveling abroad to the United States so he wrote a program to convert fahrenheit to celsius. Unfortunately his code has some bugs.
 */
