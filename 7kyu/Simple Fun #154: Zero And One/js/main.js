22393c1c223bbdf4ef45ba3a1418bd258ca0018e
widezad <Widezad@gmail.com>
2022-10-18 18:49:07 -0400

    New Challenge

--- 7kyu/Simple Fun #154: Zero And One/js/main.js
+++ 7kyu/Simple Fun #154: Zero And One/js/main.js
@@ -4,4 +4,6 @@ DESCRIPTION:
 You have a string that consists of zeroes and ones. Now choose any two 
 adjacent positions in the string: if one of them is 0, and the other 
 one is 1, remove these two digits from the string.
+
+Return the length of the resulting (smallest) string that you can get after applying this operation multiple times?
 */
