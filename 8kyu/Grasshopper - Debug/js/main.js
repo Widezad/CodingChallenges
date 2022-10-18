2d81096e93f4411afbd2abd94fcb67575a6085f1
widezad <Widezad@gmail.com>
2022-10-17 22:44:17 -0400

    New Challenge

--- 8kyu/Grasshopper - Debug/js/main.js
+++ 8kyu/Grasshopper - Debug/js/main.js
@@ -7,5 +7,6 @@ Your friend is traveling abroad to the United States so he wrote a
 program to convert fahrenheit to celsius. Unfortunately his code has 
 some bugs.
 
-Find the errors in the code to get the celsius converter working properly.
+Find the errors in the code to get the celsius converter working 
+properly.
 */
