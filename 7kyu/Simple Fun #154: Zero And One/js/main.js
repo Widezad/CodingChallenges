a0a91b0dc4723af585e9b77496e8c9c7a94a39a1
widezad <Widezad@gmail.com>
2022-10-18 18:56:22 -0400

    New Challenge

--- 7kyu/Simple Fun #154: Zero And One/js/main.js
+++ 7kyu/Simple Fun #154: Zero And One/js/main.js
@@ -42,3 +42,4 @@ console.log(zeroAndOne("01010")) //1
 console.log(zeroAndOne("11101111")) //6
 console.log(zeroAndOne("01")) //0
 console.log(zeroAndOne("10")) //0
+console.log(zeroAndOne("110110")) //2
