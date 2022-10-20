890ea53a14f7d34cec44cdf036bb5c7f65c9ce86
widezad <Widezad@gmail.com>
2022-10-19 21:22:14 -0400

    New Challenge

--- 7kyu/Isograms/js/main.js
+++ 7kyu/Isograms/js/main.js
@@ -3,5 +3,6 @@ DESCRIPTION:
 
 An isogram is a word that has no repeating letters, consecutive 
 or non-consecutive. Implement a function that determines whether 
-a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+a string that contains only letters is an isogram. Assume the 
+empty string is an isogram. Ignore letter case.
 */
