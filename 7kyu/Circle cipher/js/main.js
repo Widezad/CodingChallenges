3ccb2b1255857a4e76d29b6fe8712dc0e58a3d4c
widezad <Widezad@gmail.com>
2022-10-20 22:35:02 -0400

    New Challenge

--- 7kyu/Circle cipher/js/main.js
+++ 7kyu/Circle cipher/js/main.js
@@ -3,4 +3,6 @@ DESCRIPTION:
 
 magine a circle. To encode the word codewars, we could split the circle 
 into 8 parts (as codewars has 8 letters):
+
+Then add the letters in a clockwise direction:
 */
