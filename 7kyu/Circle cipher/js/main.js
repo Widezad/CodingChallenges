b386022c49dfd77c95efac5ec6a594e2f36a4c77
widezad <Widezad@gmail.com>
2022-10-20 22:56:11 -0400

    New Challenge

--- 7kyu/Circle cipher/js/main.js
+++ 7kyu/Circle cipher/js/main.js
@@ -38,3 +38,4 @@ console.log(decode("csordaew")) //codewars
 console.log(decode("wehti")) //"white"
 console.log(decode("Atsrse")) // "Assert"
 console.log(decode("H!edlllroo w")) //"Hello world!"
+console.log(decode("Y.oaut ahka vsei hcth oesteanl stnoa rt")) //You have chosen to translate this kata.
