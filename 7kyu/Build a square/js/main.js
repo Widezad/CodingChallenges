c403143ed1cb9336b190c79be93a8e61b5594974
widezad <Widezad@gmail.com>
2022-10-21 22:36:26 -0400

    New Challenge

--- 7kyu/Build a square/js/main.js
+++ 7kyu/Build a square/js/main.js
@@ -1,5 +1,6 @@
 /*
 DESCRIPTION:
 
-I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+I will give you an integer. Give me back a shape that is as long and 
+wide as the integer. The integer will be a whole number between 1 and 50.
 */
