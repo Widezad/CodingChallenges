c2e29d7f5304a57121c1f79c76e5031c79d6b948
widezad <Widezad@gmail.com>
2022-10-22 21:20:14 -0400

    New Challenge

--- 8kyu/Holiday VIII - Duty Free/js/main.js
+++ 8kyu/Holiday VIII - Duty Free/js/main.js
@@ -3,5 +3,6 @@ DESCRIPTION:
 
 The purpose of this kata is to work out just how many bottles of 
 duty free whiskey you would have to buy such that the saving over 
-the normal high street price would effectively cover the cost of your holiday.
+the normal high street price would effectively cover the cost of 
+your holiday.
 */
