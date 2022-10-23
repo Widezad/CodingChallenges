da6eb43c7b13f43b1a0da4726319d8e8e085ff95
widezad <Widezad@gmail.com>
2022-10-22 21:21:46 -0400

    New Challenge

--- 8kyu/Holiday VIII - Duty Free/js/main.js
+++ 8kyu/Holiday VIII - Duty Free/js/main.js
@@ -9,5 +9,6 @@ your holiday.
 You will be given the high street price (normPrice), the duty free 
 discount (discount) and the cost of the holiday.
 
-For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
+For example, if a bottle cost £10 normally and the discount in duty 
+free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
 */
