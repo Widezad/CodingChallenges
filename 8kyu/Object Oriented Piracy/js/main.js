ece319f6040a19d2aec8ea9e1143c69eab7bd000
widezad <Widezad@gmail.com>
2022-10-24 11:16:12 -0400

    New Challenge

--- 8kyu/Object Oriented Piracy/js/main.js
+++ 8kyu/Object Oriented Piracy/js/main.js
@@ -47,6 +47,8 @@ Good luck!
 function Ship(draft,crew) {
     this.draft = draft;
     this.crew = crew;
+    this.isWorthIt = function () {
+        return this.draft - this.crew * 1.5 > 20
+    }
 }
-   
 //Test Cases
