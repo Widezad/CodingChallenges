a8e5f3aa6e2dd7245b08f104bc29e20d5c68d83d
widezad <Widezad@gmail.com>
2022-10-24 11:05:25 -0400

    New Challenge

--- 8kyu/Object Oriented Piracy/js/main.js
+++ 8kyu/Object Oriented Piracy/js/main.js
@@ -4,5 +4,6 @@ DESCRIPTION:
 Ahoy matey!
 
 ou are a leader of a small pirate crew. And you have a plan. With the 
-help of OOP you wish to make a pretty efficient system to identify ships with a heavy booty on board.
+help of OOP you wish to make a pretty efficient system to identify ships 
+with a heavy booty on board.
 */
