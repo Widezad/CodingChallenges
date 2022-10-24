9a3727c27544af773a0c3bb966710417a208b9ba
widezad <Widezad@gmail.com>
2022-10-23 21:41:38 -0400

    New Challenge

--- 8kyu/Duck Duck Goose/js/main.js
+++ 8kyu/Duck Duck Goose/js/main.js
@@ -6,5 +6,6 @@ tapping on each player's head until one is chosen.
 
 Task: Given an array of Player objects (an array of associative 
 arrays in PHP) and an index (1-based), return the name of the 
-chosen Player(name is a property of Player objects, e.g Player.name)
+chosen Player(name is a property of Player objects, 
+e.g Player.name)
 */
