2ce270611b1886a11dcd5ea14d82a219b758378e
widezad <Widezad@gmail.com>
2022-10-26 16:56:55 -0400

    New Challenge

--- 8kyu/Count Odd Numbers below n/js/main.js
+++ 8kyu/Count Odd Numbers below n/js/main.js
@@ -6,6 +6,7 @@ below n, EASY!
 
 Examples(input -> Output) 
 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
-15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 
+11, 13])
 
 */
