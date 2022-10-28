c2f8a355b87c1eb8c82780e84ead6ee13f930056
widezad <Widezad@gmail.com>
2022-10-28 14:57:37 -0400

    New Challenge

--- 8kyu/pick a set of first elements/js/main.js
+++ 8kyu/pick a set of first elements/js/main.js
@@ -4,4 +4,6 @@ DESCRIPTION:
 Write a function to get the first element(s) of a sequence. 
 Passing a parameter n (default=1) will return the first n 
 element(s) of the sequence.
+
+if n == 0 return an empty sequence []
 */
