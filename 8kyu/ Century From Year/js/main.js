abf403cdf9c36531ff5d0ce05a03aaa8f7c8a86e
widezad <Widezad@gmail.com>
2022-10-29 20:01:45 -0400

    New Challenge

--- 8kyu/ Century From Year/js/main.js
+++ 8kyu/ Century From Year/js/main.js
@@ -28,3 +28,4 @@ function century (year) {
 console.log(century(1705)) //18 "Testing for year 1705"
 console.log(century(1900)) //19 "Testing for year 1900"
 console.log(century(1601)) //17 "Testing for year 1601"
+console.log(century(2000)) //20 "Testing for year 2000"
