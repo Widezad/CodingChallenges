c2e6766155d5c1ff7c6d697893188e5581241807
widezad <Widezad@gmail.com>
2022-10-30 21:03:34 -0400

    New Challenge

--- 8kyu/Return the day/js/main.js
+++ 8kyu/Return the day/js/main.js
@@ -23,3 +23,4 @@ function whatday(num) {
 console.log(whatday(1)) // 'Sunday'
 console.log(whatday(2)) //'Monday'
 console.log(whatday(3)) // 'Tuesday'
+console.log(whatday(8)) // 'Wrong, please enter a number between 1 snd 7'
