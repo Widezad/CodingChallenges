a9b9441ca62837d43da0df1d0470679059a427e3
widezad <Widezad@gmail.com>
2022-10-31 22:41:15 -0400

    New Challenge

--- 8kyu/Twice as old/js/main.js
+++ 8kyu/Twice as old/js/main.js
@@ -8,5 +8,6 @@ Your function takes two arguments
 
 Сalculate how many years ago the father was twice as old as his son 
 (or in how many years he will be twice as old). The answer is 
-always greater or equal to 0, no matter if it was in the past or it is in the future.
+always greater or equal to 0, no matter if it was in the past or 
+it is in the future.
 */
