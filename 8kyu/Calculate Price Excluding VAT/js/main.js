c58915c349fd7e8d7194bba6ccd92b0031950585
widezad <Widezad@gmail.com>
2022-11-01 15:54:08 -0400

    New Challenge

--- 8kyu/Calculate Price Excluding VAT/js/main.js
+++ 8kyu/Calculate Price Excluding VAT/js/main.js
@@ -6,5 +6,6 @@ without VAT.
 
 Example:
 
-If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+If a product price is 200.00 and VAT is 15%, then the final 
+product price (with VAT) is: 200.00 + 15% = 230.00
 */
