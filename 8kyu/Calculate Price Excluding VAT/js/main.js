ee2143c3ed6b69a38f0771d88652b0f1c17ee48b
widezad <Widezad@gmail.com>
2022-11-01 15:56:33 -0400

    New Challenge

--- 8kyu/Calculate Price Excluding VAT/js/main.js
+++ 8kyu/Calculate Price Excluding VAT/js/main.js
@@ -7,7 +7,7 @@ without VAT.
 Example:
 
 If a product price is 200.00 and VAT is 15%, then the final 
-product price (with VAT) is: 200.00 + 15% = 230.00
+product price (with VAT) is: 200.00 + 15% = 230.00)
 
 Thus, if your function receives 230.00 as input, it should 
 return 200.00
@@ -16,4 +16,5 @@ Notes:
 
 VAT is always 15% for the purposes of this kata.
 Roud the result to 2 decimal places.
+if null value given then return -1
 */
