b3e47e25930415e65b640bf726401084d51447e8
widezad <Widezad@gmail.com>
2022-10-31 22:45:36 -0400

    New Challenge

--- 8kyu/Twice as old/js/main.js
+++ 8kyu/Twice as old/js/main.js
@@ -20,3 +20,4 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 console.log(twiceAsOld(36, 7)) //22
 console.log(twiceAsOld(55, 30)) //5
 console.log(twiceAsOld(22, 1)) //20
+console.log(twiceAsOld(29, 0)) //29
