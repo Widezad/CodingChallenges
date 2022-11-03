aba631832bfcef246b79417c80411e77f9b41777
widezad <Widezad@gmail.com>
2022-11-02 22:50:53 -0400

    New Challenge

--- 8kyu/Localize The Barycenter of a Triangle/js/main.js
+++ 8kyu/Localize The Barycenter of a Triangle/js/main.js
@@ -40,3 +40,4 @@ function barTriang(p1, p2, p3) {
 }
 //Test cases
 console.log(barTriang([4, 6], [12, 4], [10, 10])) //[8.6667, 6.6667]
+console.log(barTriang([4, 2], [12, 2], [6, 10])) //[7.3333, 4.6667]
