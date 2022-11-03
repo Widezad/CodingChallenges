7cc167c08d65ec47d88ab6f5ad2d9a9b269e7c3f
widezad <Widezad@gmail.com>
2022-11-02 22:37:34 -0400

    New Challenge

--- 8kyu/Localize The Barycenter of a Triangle/js/main.js
+++ 8kyu/Localize The Barycenter of a Triangle/js/main.js
@@ -6,4 +6,6 @@ with the midpoint of their opposite sides. The three medians of a
 triangle intersect at the same point, called the barycenter or the 
 centroid. Given a triangle, defined by the cartesian coordinates of 
 its vertices we need to localize its barycenter or centroid.
+
+The function bar_triang() or barTriang or bar-triang, receives the coordinates of the three vertices A, B and C  as three different arguments and outputs the coordinates of the barycenter O in an array [xO, yO]
 */
