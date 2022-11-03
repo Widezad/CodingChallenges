b995443170251e8f017fb8ae7ccc92aa071fb675
widezad <Widezad@gmail.com>
2022-11-02 22:35:27 -0400

    New Challenge

--- 8kyu/Localize The Barycenter of a Triangle/js/main.js
+++ 8kyu/Localize The Barycenter of a Triangle/js/main.js
@@ -2,5 +2,6 @@
 DESCRIPTION:
 
 The medians of a triangle are the segments that unit the vertices 
-with the midpoint of their opposite sides. The three medians of a triangle intersect at the same point, called the barycenter or the centroid. Given a triangle, defined by the cartesian coordinates of its vertices we need to localize its barycenter or centroid.
+with the midpoint of their opposite sides. The three medians of a 
+triangle intersect at the same point, called the barycenter or the centroid. Given a triangle, defined by the cartesian coordinates of its vertices we need to localize its barycenter or centroid.
 */
