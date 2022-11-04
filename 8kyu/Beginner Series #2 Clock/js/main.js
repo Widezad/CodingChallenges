6b29f783d4efd73c0458b2bb10e3782e570dd1c9
widezad <Widezad@gmail.com>
2022-11-03 22:05:48 -0400

    New Challenge

--- 8kyu/Beginner Series #2 Clock/js/main.js
+++ 8kyu/Beginner Series #2 Clock/js/main.js
@@ -3,5 +3,6 @@ DESCRIPTION:
 
 Clock shows h hours, m minutes and s seconds after midnight.
 
-Your task is to write a function which returns the time since midnight in milliseconds.
+Your task is to write a function which returns the time since midnight 
+in milliseconds.
 */
