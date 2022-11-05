92819d29c778c1c610b6a6424849b4bde1cca72e
widezad <Widezad@gmail.com>
2022-11-04 22:58:58 -0400

    New Challenge

--- 8kyu/ Is it a palindrome?/js/main.js
+++ 8kyu/ Is it a palindrome?/js/main.js
@@ -5,6 +5,6 @@ Write a function that checks if a given string
 
 //Function
 function isPalindrome(x) {
-    
+    return x.roLowerCase() == x.tolowercase().split('').reverse().join('')
 }
 //Test Cases
